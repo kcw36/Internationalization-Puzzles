c1e5d0c60d924342befd8c0a1ff68a56ddf95fe7
kcw36 <kylewatt2000@hotmail.com>
2025-03-28 01:13:47 +0000

    Add data extraction for txt inputs

--- length_limits.py
+++ length_limits.py
@@ -1,5 +1,7 @@
 """Take message input and calculate cost for message"""
 
+import os
+
 def get_message_cost(messages: list[str]) -> int:
     """Return message cost based on content"""
     cost = 0
@@ -13,8 +15,18 @@ def get_message_cost(messages: list[str]) -> int:
         elif m_char <= 140:
             cost += 7
     return cost
+
+
+def get_list_from_txt(path_to_file: str) -> list[str]:
+    """Return list of strings from txt file"""
+    absolute_path = os.path.dirname(__file__)
+    path = f"{absolute_path}/{path_to_file}"
+    with open(path, "r") as f:
+        text = f.read()
+    return text.split("\n")
             
 
 if __name__ == "__main__":
-    message = input("Please input message to check: ")
+    message = get_list_from_txt("input.txt")
     msg_cost = get_message_cost(message)
+    print(msg_cost)
--- tests_length_limits.py
+++ tests_length_limits.py
@@ -1,6 +1,5 @@
 # pylint: skip-file
 
-import pytest
 from length_limits import get_message_cost
 
 def test_get_message_cost_valid(test_message):
