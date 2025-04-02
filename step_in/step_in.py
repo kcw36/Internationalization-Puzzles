454e250459dc70af36b40959f577efb91d28466b
kcw36 <kylewat2000@hotmail.com>
2025-04-02 10:47:37 +0100

    Add step in logic

--- step_in/step_in.py
+++ step_in/step_in.py
@@ -1,11 +1,22 @@
 """Calculate number of poo hits in a park."""
 
-
 from os import path
 
 
-def get_poos_hit(park: list[list]) -> int:
+def get_poo_hit(park: list[list]) -> int:
     """Return number of poos hit in park."""
+    x = 0
+    y = 0
+    poo_count = 0
+    columns = len(park[0])
+    while y < len(park):
+        if park[y][x] == "0x01f4a9":
+            poo_count += 1
+        x += 2
+        if x >= columns:
+            x = x - columns
+        y += 1
+    return poo_count
 
 
 def get_park_from_txt(path_to_file: str) -> list[list]:
@@ -14,11 +25,14 @@ def get_park_from_txt(path_to_file: str) -> list[list]:
     rel_path = f"{absolute_path}/{path_to_file}"
     park = []
     with open(rel_path, "r", encoding="utf-8") as f:
-        for line in f:
+        # removes the \n char from line end
+        data = f.read().splitlines()
+        for line in data:
             unicode_chars = [format(ord(c), '#08x') for c in line]
             park.append(unicode_chars)
     return park
 
 
 if __name__ == "__main__":
-    print(get_park_from_txt("test_input.txt"))
+    park = get_park_from_txt("input.txt")
+    print(get_poo_hit(park))
--- step_in/test_step_in.py
+++ step_in/test_step_in.py
@@ -1,7 +1,7 @@
 # pylint: skip-file
 
-from step_in.step_in import get_poos_hit
+from step_in.step_in import get_poo_hit
 
 
 def test_get_poos_hit(test_park):
-    assert 2 == get_poos_hit(test_park)
+    assert 2 == get_poo_hit(test_park)
